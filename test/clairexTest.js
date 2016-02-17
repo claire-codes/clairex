2591381c3c83a3392a257e609930c8ab10828a6e
Claire Parker <claire.parker87@gmail.com>
2016-02-17 15:42:27 +0200

    :candy: Working vanilla mocha test

---
+++ test/clairexTest.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var clairex = require("../lib/clairex");
+
+describe("it should work", function() {
+  it("really should", function() {
+    expect(clairex.getFoo()).to.eq(42);
+  });
+});
