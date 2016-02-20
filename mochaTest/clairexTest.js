18e374fa63d531d9901c0a3a77ed2f168328bb44
Claire Parker <claire.parker87@gmail.com>
2016-02-20 20:33:07 +0000

    :articulated_lorry: Proving vanilla versions work too

--- mochaTest/clairexTest.js
+++ mochaTest/clairexTest.js
@@ -6,3 +6,17 @@ describe("it should work", function() {
     expect(clairex.getFoo()).to.eq(42);
   });
 });
+
+describe("#squeeze", function() {
+  it("should work", function() {
+    expect(clairex.squeeze('yellow moon')).to.eq('yelow mon');
+    expect(clairex.squeeze('yellow moon')).to.eq('yelow mon');
+    expect(clairex.squeeze('putters shoot balls','m-z')).to.eq('puters shot balls');
+  });
+});
+
+describe("#changeMoney", function() {
+  it("should work", function() {
+    expect(clairex.changeMoney('£5')).to.eq('5 pounds');
+  });
+});
