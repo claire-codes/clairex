5e4b87b230fac68bf9b92dfdfb2272369c3e3149
Claire Parker <claire.parker87@gmail.com>
2016-04-20 22:07:38 +0100

    :keycap_ten: Playing with chaining

--- src/chain.js
+++ src/chain.js
@@ -1,3 +1,23 @@
-let getChain = () => 24;
+let chainObj = {
+    phrase: "",
+    
+    shoutIt: function() {
+        if (this.phrase) {
+            this.phrase = this.phrase.toUpperCase();
+        }
+        return this;
+    },
+    
+    makeItRed: function(noun) {
+        this.phrase = "red " + noun;
+        return this;
+    },
+    
+    val: function() {
+        var tmp = this.phrase;
+        this.phrase = "";
+        return tmp;
+    }
+}
 
-export { getChain }
+export { chainObj }
--- test/chainTest.js
+++ test/chainTest.js
@@ -1,7 +1,10 @@
 import test from 'ava';
 import 'babel-core/register';
-import { getChain } from '../src/chain';
+import { chainObj } from '../src/chain';
 
 test('chain', t => {
-  t.same(24, getChain());
+  t.same('red apple', chainObj.makeItRed('apple').val());
+  t.same('', chainObj.shoutIt().val());
+  t.same('RED VINE', chainObj.makeItRed('vine').shoutIt().val());
+  t.same('red banana', chainObj.shoutIt().makeItRed('banana').val());
 });
