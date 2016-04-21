7f872419924f255ddb7de6f4f0897058b08fc9df
Claire Parker <claire.parker87@gmail.com>
2016-04-21 21:42:38 +0100

    :tv: Using prototypes for chaining instead

--- src/chain.js
+++ src/chain.js
@@ -1,18 +1,18 @@
 let chainObj = {
     phrase: "",
-    
+
     shoutIt: function() {
         if (this.phrase) {
             this.phrase = this.phrase.toUpperCase();
         }
         return this;
     },
-    
+
     makeItRed: function(noun) {
         this.phrase = "red " + noun;
         return this;
     },
-    
+
     val: function() {
         var tmp = this.phrase;
         this.phrase = "";
@@ -20,4 +20,30 @@ let chainObj = {
     }
 }
 
-export { chainObj }
+let ChainPrototype = function() {
+    this.phrase = "";
+}
+
+ChainPrototype.prototype.setPhrase = function(noun) {
+    this.phrase = noun;
+    return this;
+};
+
+ChainPrototype.prototype.makeItRed = function() {
+    if (this.phrase) {
+        this.phrase = "red " + this.phrase;
+    }
+    return this;
+};
+
+ChainPrototype.prototype.shoutIt = function() {
+    if (this.phrase) {
+        this.phrase = this.phrase.toUpperCase();
+    }
+    return this;
+};
+
+export {
+    chainObj,
+    ChainPrototype
+}
--- test/chainTest.js
+++ test/chainTest.js
@@ -1,10 +1,20 @@
 import test from 'ava';
 import 'babel-core/register';
-import { chainObj } from '../src/chain';
+import { chainObj, ChainPrototype } from '../src/chain';
 
-test('chain', t => {
+test('chaining an Object', t => {
   t.same('red apple', chainObj.makeItRed('apple').val());
   t.same('', chainObj.shoutIt().val());
   t.same('RED VINE', chainObj.makeItRed('vine').shoutIt().val());
   t.same('red banana', chainObj.shoutIt().makeItRed('banana').val());
 });
+
+test('chaining a prototype', t => {
+    let sentence = new ChainPrototype();
+    t.same('apple', sentence.setPhrase('apple').phrase);
+    t.same('red eye', sentence.setPhrase('eye').makeItRed().phrase);
+    t.same('RED BALLOONS', sentence.setPhrase('balloons').makeItRed().shoutIt().phrase);
+    t.same('red BALLOONS', sentence.setPhrase('balloons').shoutIt().makeItRed().phrase);
+    sentence = new ChainPrototype();
+    t.same('', sentence.shoutIt().phrase);
+})
