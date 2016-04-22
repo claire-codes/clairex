6fec355f61d8f1c1f8cc683693f21196ba36da1f
Claire Parker <claire.parker87@gmail.com>
2016-04-22 20:22:15 +0100

    :white_small_square: Use ava-codemods to rename same -> deepEqual

--- test/avaTest.js
+++ test/avaTest.js
@@ -11,60 +11,60 @@ import {
 } from "../src/index";
 
 test("clairex", t => {
-    t.same(42, getFoo());
+    t.deepEqual(42, getFoo());
 });
 
 test("replaceWhitespace", t => {
-    t.same(replaceWhitespace("foo bar", "-"), "foo-bar");
-    t.same(replaceWhitespace("bish bash bosh", "!"), "bish!bash!bosh");
-    t.same(replaceWhitespace("flim  flam   floop", "*"), "flim**flam***floop");
-    t.same(replaceWhitespace(" braap  ", "$"), "$braap$$");
-    t.same(replaceWhitespace("oof", "%"), "oof");
-    t.same(replaceWhitespace("good golly", ""), "goodgolly");
-    t.same(replaceWhitespace("up side your head"), "upsideyourhead");
+    t.deepEqual(replaceWhitespace("foo bar", "-"), "foo-bar");
+    t.deepEqual(replaceWhitespace("bish bash bosh", "!"), "bish!bash!bosh");
+    t.deepEqual(replaceWhitespace("flim  flam   floop", "*"), "flim**flam***floop");
+    t.deepEqual(replaceWhitespace(" braap  ", "$"), "$braap$$");
+    t.deepEqual(replaceWhitespace("oof", "%"), "oof");
+    t.deepEqual(replaceWhitespace("good golly", ""), "goodgolly");
+    t.deepEqual(replaceWhitespace("up side your head"), "upsideyourhead");
 });
 
 test("convertMiscChar", t => {
-    t.same(convertMiscChar("Me & You"), "Me and You");
-    t.same(convertMiscChar("#42"), "number 42");
-    t.same(convertMiscChar("110%"), "110 percent");
-    t.same(convertMiscChar("a/b"), "a slash b");
-    t.same(convertMiscChar("a\\b"), "a slash b");
-    t.same(convertMiscChar("*this*"), " star this star ");
-    t.same(convertMiscChar("claire@parker"), "claire at parker");
+    t.deepEqual(convertMiscChar("Me & You"), "Me and You");
+    t.deepEqual(convertMiscChar("#42"), "number 42");
+    t.deepEqual(convertMiscChar("110%"), "110 percent");
+    t.deepEqual(convertMiscChar("a/b"), "a slash b");
+    t.deepEqual(convertMiscChar("a\\b"), "a slash b");
+    t.deepEqual(convertMiscChar("*this*"), " star this star ");
+    t.deepEqual(convertMiscChar("claire@parker"), "claire at parker");
 });
 
 test("convertPunctuation", t => {
-    t.same(convertPunctuation("{ 2 + 2 }"), "brace 2 + 2 brace");
-    t.same(convertPunctuation("(or you)"), " bracket or you bracket ");
-    t.same(convertPunctuation("it\'s"), "its");
-    t.same(convertPunctuation("\"wow\""), "wow");
-    t.same(convertPunctuation("er, no"), "er no");
-    t.same(convertPunctuation("wow!!!"), "wow");
-    t.same(convertPunctuation("huh?"), "huh");
-    t.same(convertPunctuation("package.json?"), "package dot json");
-    t.same(convertPunctuation("Mr. Jones.com"), "Mr Jones dot com");
+    t.deepEqual(convertPunctuation("{ 2 + 2 }"), "brace 2 + 2 brace");
+    t.deepEqual(convertPunctuation("(or you)"), " bracket or you bracket ");
+    t.deepEqual(convertPunctuation("it\'s"), "its");
+    t.deepEqual(convertPunctuation("\"wow\""), "wow");
+    t.deepEqual(convertPunctuation("er, no"), "er no");
+    t.deepEqual(convertPunctuation("wow!!!"), "wow");
+    t.deepEqual(convertPunctuation("huh?"), "huh");
+    t.deepEqual(convertPunctuation("package.json?"), "package dot json");
+    t.deepEqual(convertPunctuation("Mr. Jones.com"), "Mr Jones dot com");
 });
 
 test("#squeeze", t => {
-    t.same("yelow mon", squeeze("yellow moon"));
-    t.same(" now is the", squeeze("  now   is  the"));
-    t.same("puters shot balls", squeeze("putters shoot balls", "m-z"));
-    t.same("just some spaaace ", squeeze("just   some   spaaace  ", " "));
+    t.deepEqual("yelow mon", squeeze("yellow moon"));
+    t.deepEqual(" now is the", squeeze("  now   is  the"));
+    t.deepEqual("puters shot balls", squeeze("putters shoot balls", "m-z"));
+    t.deepEqual("just some spaaace ", squeeze("just   some   spaaace  ", " "));
 });
 
 test("#changeMoney pounds only", t => {
-    t.same("2 pounds", changeMoney("£2"));
-    t.same("4 dollars", changeMoney("$4"));
-    t.same("6 euro", changeMoney("€6"));
+    t.deepEqual("2 pounds", changeMoney("£2"));
+    t.deepEqual("4 dollars", changeMoney("$4"));
+    t.deepEqual("6 euro", changeMoney("€6"));
 });
 
 test("#changeMoney pounds and pennies", t => {
-    t.same("2 pounds 34 pence", changeMoney("£2.34"));
-    t.same("4 dollars 56 cents", changeMoney("$4.56"));
-    t.same("6 euro 78 cents", changeMoney("€6.78"));
+    t.deepEqual("2 pounds 34 pence", changeMoney("£2.34"));
+    t.deepEqual("4 dollars 56 cents", changeMoney("$4.56"));
+    t.deepEqual("6 euro 78 cents", changeMoney("€6.78"));
 });
 
 test("toUrl", t => {
-    t.same("bob-and-alices-5-pounds-slash-10-dollars-trip", toUrl("Bob & Alice\"s   £5/$10 trip? "));
+    t.deepEqual("bob-and-alices-5-pounds-slash-10-dollars-trip", toUrl("Bob & Alice\"s   £5/$10 trip? "));
 });
--- test/chainTest.js
+++ test/chainTest.js
@@ -6,21 +6,21 @@ import {
 } from "../src/chain";
 
 test("chaining an Object", t => {
-    t.same("panda", chainObj.setPhrase("panda").val());
-    t.same("red apple", chainObj.setPhrase("apple").makeItRed().val());
-    t.same("red red panda", chainObj.setPhrase("panda").makeItRed().makeItRed().val());
-    t.same("", chainObj.shoutIt().val());
-    t.same("RED VINE", chainObj.setPhrase("vine").makeItRed().shoutIt().val());
-    t.same("red BANANA", chainObj.setPhrase("banana").shoutIt().makeItRed().val());
+    t.deepEqual("panda", chainObj.setPhrase("panda").val());
+    t.deepEqual("red apple", chainObj.setPhrase("apple").makeItRed().val());
+    t.deepEqual("red red panda", chainObj.setPhrase("panda").makeItRed().makeItRed().val());
+    t.deepEqual("", chainObj.shoutIt().val());
+    t.deepEqual("RED VINE", chainObj.setPhrase("vine").makeItRed().shoutIt().val());
+    t.deepEqual("red BANANA", chainObj.setPhrase("banana").shoutIt().makeItRed().val());
 });
 
 test("chaining a prototype", t => {
     let sentence = new ChainPrototype();
-    t.same("apple", sentence.setPhrase("apple").phrase);
-    t.same("red eye", sentence.setPhrase("eye").makeItRed().phrase);
-    t.same("red red bush", sentence.setPhrase("bush").makeItRed().makeItRed().phrase);
-    t.same("RED BALLOONS", sentence.setPhrase("balloons").makeItRed().shoutIt().phrase);
-    t.same("red BALLOONS", sentence.setPhrase("balloons").shoutIt().makeItRed().phrase);
+    t.deepEqual("apple", sentence.setPhrase("apple").phrase);
+    t.deepEqual("red eye", sentence.setPhrase("eye").makeItRed().phrase);
+    t.deepEqual("red red bush", sentence.setPhrase("bush").makeItRed().makeItRed().phrase);
+    t.deepEqual("RED BALLOONS", sentence.setPhrase("balloons").makeItRed().shoutIt().phrase);
+    t.deepEqual("red BALLOONS", sentence.setPhrase("balloons").shoutIt().makeItRed().phrase);
     sentence = new ChainPrototype();
-    t.same("", sentence.shoutIt().phrase);
+    t.deepEqual("", sentence.shoutIt().phrase);
 });
