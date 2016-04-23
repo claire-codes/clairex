82696b1ef21477a490274c02a67e7ce1f57d7f37
Claire Parker <claire.parker87@gmail.com>
2016-04-23 22:47:51 +0100

    :link: Make all functions chainable except toUrl()

--- src/clairendex.js
+++ src/clairendex.js
@@ -1,82 +1,104 @@
-let getFoo = () => 42;
+let clairex = {
 
-let replaceWhitespace = (str, replacement = "") => {
-    return str.replace(new RegExp("\\s", "g"), replacement);
-};
+    ex: "",
 
-let convertMiscChar = str => {
-    let exps = [
-        {char: /&/g, replace: "and"},
-        {char: /#/g, replace: "number "},
-        {char: /%/g, replace: " percent"},
-        {char: /\/|\\/g, replace: " slash "},
-        {char: /\*/g, replace: " star "},
-        {char: /@/g, replace: " at "}
-        // brackets
-    ];
-    for (let i = 0; i < exps.length; i++) {
-        str = str.replace(new RegExp(exps[i].char), exps[i].replace);
-    }
-    return str;
-};
+    getFoo: function() {
+        return 42;
+    },
 
-let convertPunctuation = str => {
-    let exps = [
-        {punc: "\\{|\\}", replace: "brace"},
-        {punc: "\\(|\\)", replace: " bracket "},
-        {punc: "\'", replace: ""},
-        {punc: "\"", replace: ""},
-        {punc: ",", replace: ""},
-        {punc: "\(\\w\)\\.\(\\s\)", replace: "$1$2"},
-        {punc: "\\.", replace: " dot "},
-        {punc: "!", replace: ""},
-        {punc: "\\?", replace: ""}
-    ];
-    for (let i = 0; i < exps.length; i++) {
-        str = str.replace(new RegExp(exps[i].punc, "g"), exps[i].replace);
-    }
-    return str;
-};
+    set: function(str) {
+        this.ex = str;
+        return this;
+    },
 
-let squeeze = (str, range) => {
-    let chars = [...str];
-    let rangeRegexp = new RegExp(`[${range}]`, "g");
-    for (let j = 0; j < chars.length; j++) {
-        if (chars[j] === chars[j+1]) {
-            // if range === undefined i.e. false
-            if (!range) {
-                chars[j] = "";
-            } else if (chars[j].match(rangeRegexp)) {
-                chars[j] = "";
+    replaceWhitespace: function(replacement = "") {
+        this.ex = this.ex.replace(new RegExp("\\s", "g"), replacement);
+        return this;
+    },
+
+    convertMiscChar: function() {
+        let exps = [
+            {char: /&/g, replace: "and"},
+            {char: /#/g, replace: "number "},
+            {char: /%/g, replace: " percent"},
+            {char: /\/|\\/g, replace: " slash "},
+            {char: /\*/g, replace: " star "},
+            {char: /@/g, replace: " at "}
+            // brackets
+        ];
+        for (let i = 0; i < exps.length; i++) {
+            this.ex = this.ex.replace(new RegExp(exps[i].char), exps[i].replace);
+        }
+        return this;
+    },
+
+    convertPunctuation: function() {
+        let exps = [
+            {punc: "\\{|\\}", replace: "brace"},
+            {punc: "\\(|\\)", replace: " bracket "},
+            {punc: "\'", replace: ""},
+            {punc: "\"", replace: ""},
+            {punc: ",", replace: ""},
+            {punc: "\(\\w\)\\.\(\\s\)", replace: "$1$2"},
+            {punc: "\\.", replace: " dot "},
+            {punc: "!", replace: ""},
+            {punc: "\\?", replace: ""}
+        ];
+        for (let i = 0; i < exps.length; i++) {
+            this.ex = this.ex.replace(new RegExp(exps[i].punc, "g"), exps[i].replace);
+        }
+        return this;
+    },
+
+    squeeze: function(range) {
+        let chars = [...this.ex];
+        let rangeRegexp = new RegExp(`[${range}]`, "g");
+        for (let j = 0; j < chars.length; j++) {
+            if (chars[j] === chars[j+1]) {
+                // if range === undefined i.e. false
+                if (!range) {
+                    chars[j] = "";
+                } else if (chars[j].match(rangeRegexp)) {
+                    chars[j] = "";
+                }
             }
         }
-    }
-    return chars.join("");
-};
+        this.ex = chars.join("");
+        return this;
+    },
 
-let changeMoney = str => {
-    let exps = [
-        {regex: /\$(\d+).(\d\d)/g, replace: "$1 dollars $2 cents"},
-        {regex: /\$(\d+)/g, replace: "$1 dollars"},
-        {regex: /\€(\d+).(\d\d)/g, replace: "$1 euro $2 cents"},
-        {regex: /\€(\d+)/g, replace: "$1 euro"},
-        {regex: /\£(\d+).(\d\d)/g, replace: "$1 pounds $2 pence"},
-        {regex: /\£(\d+)/g, replace: "$1 pounds"}
-    ];
-    for (let i = 0; i < exps.length; i++) {
-        str = str.replace(new RegExp(exps[i].regex), exps[i].replace);
-    }
-    return str;
-};
+    changeMoney: function() {
+        let exps = [
+            {regex: /\$(\d+).(\d\d)/g, replace: "$1 dollars $2 cents"},
+            {regex: /\$(\d+)/g, replace: "$1 dollars"},
+            {regex: /\€(\d+).(\d\d)/g, replace: "$1 euro $2 cents"},
+            {regex: /\€(\d+)/g, replace: "$1 euro"},
+            {regex: /\£(\d+).(\d\d)/g, replace: "$1 pounds $2 pence"},
+            {regex: /\£(\d+)/g, replace: "$1 pounds"}
+        ];
+        for (let i = 0; i < exps.length; i++) {
+            this.ex = this.ex.replace(new RegExp(exps[i].regex), exps[i].replace);
+        }
+        return this;
+    },
 
-let toUrl = str => replaceWhitespace(changeMoney(convertPunctuation(convertMiscChar(squeeze(str.trim()," ")))), "-").toLowerCase();
+    toUrl: function(str) {
+        // I don't care about chaning this, I want to be able to call it alone and get the string returned.
+        return this.set(str.trim())
+            .squeeze(" ")
+            .convertMiscChar()
+            .convertPunctuation()
+            .changeMoney()
+            .replaceWhitespace("-")
+            .val()
+            .toLowerCase();
+    },
+
+    val: function() {
+        return this.ex;
+    }
+}
 
 export {
-    getFoo,
-    toUrl, 
-    squeeze, 
-    changeMoney, 
-    replaceWhitespace, 
-    convertMiscChar,
-    convertPunctuation 
+    clairex
 };
--- test/clairendexTest.js
+++ test/clairendexTest.js
@@ -1,70 +1,66 @@
 import test from "ava";
 import "babel-core/register";
 import {
-    getFoo,
+    clairex,
     toUrl,
-    squeeze,
-    changeMoney,
-    replaceWhitespace,
-    convertMiscChar,
-    convertPunctuation
+    squeeze
 } from "../src/clairendex";
 
 test("clairex", t => {
-    t.deepEqual(42, getFoo());
+    t.deepEqual(42, clairex.getFoo());
 });
 
 test("replaceWhitespace", t => {
-    t.deepEqual(replaceWhitespace("foo bar", "-"), "foo-bar");
-    t.deepEqual(replaceWhitespace("bish bash bosh", "!"), "bish!bash!bosh");
-    t.deepEqual(replaceWhitespace("flim  flam   floop", "*"), "flim**flam***floop");
-    t.deepEqual(replaceWhitespace(" braap  ", "$"), "$braap$$");
-    t.deepEqual(replaceWhitespace("oof", "%"), "oof");
-    t.deepEqual(replaceWhitespace("good golly", ""), "goodgolly");
-    t.deepEqual(replaceWhitespace("up side your head"), "upsideyourhead");
+    t.deepEqual(clairex.set("foo bar").replaceWhitespace("-").val(), "foo-bar");
+    t.deepEqual(clairex.set("bish bash bosh").replaceWhitespace("!").val(), "bish!bash!bosh");
+    t.deepEqual(clairex.set("flim  flam   floop").replaceWhitespace("*").val(), "flim**flam***floop");
+    t.deepEqual(clairex.set(" braap  ").replaceWhitespace("$").val(), "$braap$$");
+    t.deepEqual(clairex.set("oof").replaceWhitespace("%").val(), "oof");
+    t.deepEqual(clairex.set("good golly").replaceWhitespace("").val(), "goodgolly");
+    t.deepEqual(clairex.set("up side your head").replaceWhitespace().val(), "upsideyourhead");
 });
 
 test("convertMiscChar", t => {
-    t.deepEqual(convertMiscChar("Me & You"), "Me and You");
-    t.deepEqual(convertMiscChar("#42"), "number 42");
-    t.deepEqual(convertMiscChar("110%"), "110 percent");
-    t.deepEqual(convertMiscChar("a/b"), "a slash b");
-    t.deepEqual(convertMiscChar("a\\b"), "a slash b");
-    t.deepEqual(convertMiscChar("*this*"), " star this star ");
-    t.deepEqual(convertMiscChar("claire@parker"), "claire at parker");
+    t.deepEqual(clairex.set("Me & You").convertMiscChar().val(), "Me and You");
+    t.deepEqual(clairex.set("#42").convertMiscChar().val(), "number 42");
+    t.deepEqual(clairex.set("110%").convertMiscChar().val(), "110 percent");
+    t.deepEqual(clairex.set("a/b").convertMiscChar().val(), "a slash b");
+    t.deepEqual(clairex.set("a\\b").convertMiscChar().val(), "a slash b");
+    t.deepEqual(clairex.set("*this*").convertMiscChar().val(), " star this star ");
+    t.deepEqual(clairex.set("claire@parker").convertMiscChar().val(), "claire at parker");
 });
 
 test("convertPunctuation", t => {
-    t.deepEqual(convertPunctuation("{ 2 + 2 }"), "brace 2 + 2 brace");
-    t.deepEqual(convertPunctuation("(or you)"), " bracket or you bracket ");
-    t.deepEqual(convertPunctuation("it\'s"), "its");
-    t.deepEqual(convertPunctuation("\"wow\""), "wow");
-    t.deepEqual(convertPunctuation("er, no"), "er no");
-    t.deepEqual(convertPunctuation("wow!!!"), "wow");
-    t.deepEqual(convertPunctuation("huh?"), "huh");
-    t.deepEqual(convertPunctuation("package.json?"), "package dot json");
-    t.deepEqual(convertPunctuation("Mr. Jones.com"), "Mr Jones dot com");
+    t.deepEqual(clairex.set("{ 2 + 2 }").convertPunctuation().val(), "brace 2 + 2 brace");
+    t.deepEqual(clairex.set("(or you)").convertPunctuation().val(), " bracket or you bracket ");
+    t.deepEqual(clairex.set("it\'s").convertPunctuation().val(), "its");
+    t.deepEqual(clairex.set("\"wow\"").convertPunctuation().val(), "wow");
+    t.deepEqual(clairex.set("er, no").convertPunctuation().val(), "er no");
+    t.deepEqual(clairex.set("wow!!!").convertPunctuation().val(), "wow");
+    t.deepEqual(clairex.set("huh?").convertPunctuation().val(), "huh");
+    t.deepEqual(clairex.set("package.json?").convertPunctuation().val(), "package dot json");
+    t.deepEqual(clairex.set("Mr. Jones.com").convertPunctuation().val(), "Mr Jones dot com");
 });
 
 test("#squeeze", t => {
-    t.deepEqual("yelow mon", squeeze("yellow moon"));
-    t.deepEqual(" now is the", squeeze("  now   is  the"));
-    t.deepEqual("puters shot balls", squeeze("putters shoot balls", "m-z"));
-    t.deepEqual("just some spaaace ", squeeze("just   some   spaaace  ", " "));
+    t.deepEqual("yelow mon", clairex.set("yellow moon").squeeze().val());
+    t.deepEqual(" now is the", clairex.set("  now   is  the").squeeze().val());
+    t.deepEqual("puters shot balls", clairex.set("putters shoot balls").squeeze("m-z").val());
+    t.deepEqual("just some spaaace ", clairex.set("just   some   spaaace  ").squeeze(" ").val());
 });
 
 test("#changeMoney pounds only", t => {
-    t.deepEqual("2 pounds", changeMoney("£2"));
-    t.deepEqual("4 dollars", changeMoney("$4"));
-    t.deepEqual("6 euro", changeMoney("€6"));
+    t.deepEqual("2 pounds", clairex.set("£2").changeMoney().val());
+    t.deepEqual("4 dollars", clairex.set("$4").changeMoney().val());
+    t.deepEqual("6 euro", clairex.set("€6").changeMoney().val());
 });
 
 test("#changeMoney pounds and pennies", t => {
-    t.deepEqual("2 pounds 34 pence", changeMoney("£2.34"));
-    t.deepEqual("4 dollars 56 cents", changeMoney("$4.56"));
-    t.deepEqual("6 euro 78 cents", changeMoney("€6.78"));
+    t.deepEqual("2 pounds 34 pence", clairex.set("£2.34").changeMoney().val());
+    t.deepEqual("4 dollars 56 cents", clairex.set("$4.56").changeMoney().val());
+    t.deepEqual("6 euro 78 cents", clairex.set("€6.78").changeMoney().val());
 });
 
 test("toUrl", t => {
-    t.deepEqual("bob-and-alices-5-pounds-slash-10-dollars-trip", toUrl("Bob & Alice\"s   £5/$10 trip? "));
+    t.deepEqual("bob-and-alices-5-pounds-slash-10-dollars-trip", clairex.toUrl("Bob & Alice\"s   £5/$10 trip? "));
 });
